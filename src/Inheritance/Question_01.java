dc90079fa56d583b8d672431c429e5cbc31ebca6
asharful07 <asharfulkhan202@gmail.com>
2023-10-20 10:47:04 +0530

    Modify Question_01

--- src/Inheritance/Question_01.java
+++ src/Inheritance/Question_01.java
@@ -1,5 +1,25 @@
 package Inheritance;
 
 public class Question_01 {
+    public static void main(String[] args) {
+        B obj = new B();
+        obj.printA();
+        obj.printB();
+    }
+}
 
+//Creating a new class A
+class A{
+    int a=5;
+    void printA(){
+        System.out.println(a);
+    }
+}
+
+//Inherited from class A
+class B extends A{
+    int b=10;
+    void printB(){
+        System.out.println(b);
+    }
 }
