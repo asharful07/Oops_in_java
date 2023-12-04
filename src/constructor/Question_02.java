3c88df1576a557231bb655a7c8f854a46cd3cdf6
asharful07 <asharfulkhan202@gmail.com>
2023-12-04 22:19:22 +0530

    Question_02

---
+++ src/constructor/Question_02.java
@@ -0,0 +1,21 @@
+
+
+//Constructor example
+
+package constructor;
+class Main {
+    String languages;
+    Main(String lang) {
+        languages = lang;
+        System.out.println(languages + " Programming Language");
+    }
+public static class Question_02 {
+        public static void main(String[] args) {
+
+
+            Main obj1 = new Main("Java");
+            Main obj2 = new Main("Python");
+            Main obj3 = new Main("C");
+        }
+    }
+}
